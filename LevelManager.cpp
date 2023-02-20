4b36b29a0c63c4eacb50bb744e75a64bf741002c
SimenWol <68946439+MrGam3z@users.noreply.github.com>
2023-02-20 19:05:18 +0100

    Other: Added a few comments in LevelManager - Wanted to implement LoadLevel() & DrawLevel(), needs a bit more research into LDtk first. - Added comments to explain what each LevelState does

--- LevelManager.cpp
+++ LevelManager.cpp
@@ -1,4 +1,7 @@
 #include "LevelManager.h"
 
+// void LevelManager::LoadLevel(const int level) const {}
+// void LevelManager::DrawLevel(const int level) const {}
+
 // Sends the state of the specified level
 LevelManager::LevelState LevelManager::GetLevelState(const int level) const { return state[level]; }
--- LevelManager.h
+++ LevelManager.h
@@ -5,14 +5,18 @@ class LevelManager
 public:
 	enum LevelState
 	{
-		Closed,
-		Open,
-		Completed
+		Closed,		// Unable to play level
+		Open,		// Able to play level, never completed it
+		Completed	// Completed the level at least once, still able to play level
 	};
+private:
 public:
-	// GetLevelContents/LoadLevel --> Gets tilemap of specified level
+	// void LoadLevel(const int level) const;		// TODO: Check LDTK implementation
+	// void DrawLevel(const int level) const;
 	LevelState GetLevelState(const int level) const;
 private:
+	// int currentLevel = 0;
+
 	// Using an array to store level states. Defaults to closed.
 	static constexpr int numLevels = 1;
 	LevelState state[numLevels] = { LevelState::Closed };
