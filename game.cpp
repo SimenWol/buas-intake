f61287e3e16b8ddf868fedfa93f89992b3306862
SimenWol <68946439+MrGam3z@users.noreply.github.com>
2023-02-20 19:30:50 +0100

    Fix: Use mouse absolute position - Template used mouse relative position, changed code to use absolute mouse position. - Added a new section at the bottom of Game::Tick() for debugging purposes.

--- game.cpp
+++ game.cpp
@@ -16,9 +16,14 @@ namespace Tmpl8
 		// Multiply deltaTime by 0.001 to get deltaTime in seconds, much easier to work with
 		// By using std::min, you prevent long pauses messing up the physics
 		deltaTime = std::min(deltaTime * 0.001f, 0.05f);
-		std::cout << "Frametimer: " << deltaTime << std::endl;
 
 		// clear the graphics window
 		screen->Clear(0);
+
+
+		/* ======================== DEBUG ======================== */
+		// std::cout << "Frametimer: " << deltaTime << std::endl;						// Frametimer
+		// std::cout << "MouseX: " << mousex << " MouseY: " << mousey << std::endl;		// Mouse Position
+		/* ======================================================= */
 	}
 };
