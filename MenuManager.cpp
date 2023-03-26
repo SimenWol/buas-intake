e5df10f3f59c342c71fe894a5a234eca2dedf591
SimenWol <68946439+MrGam3z@users.noreply.github.com>
2023-03-26 20:29:22 +0200

    Other: Added temporary level selection screen - Added temporary level selection screen with functionality to the button for level 1. - Added some more comments.

--- MenuManager.cpp
+++ MenuManager.cpp
@@ -4,9 +4,11 @@
 
 // Sprites used (only) in this file
 Tmpl8::Sprite mainMenu(new Tmpl8::Surface("assets/Temp/GAME TITLE SCREEN.png"), 1);
+Tmpl8::Sprite levelSelection(new Tmpl8::Surface("assets/Temp/LEVEL SELECTION SCREEN.png"), 1);
 
 // Buttons used (only) in this file
 Button startButton(128, 373, 127, 71);
+Button levelOneButton(176, 227, 55, 57);
 
 void MenuManager::Draw(Tmpl8::Surface* screen_in) // Function that draws the required menu to the screen.
 {
@@ -14,7 +16,10 @@ void MenuManager::Draw(Tmpl8::Surface* screen_in) // Function that draws the req
 	switch (state)
 	{
 	case Main:
+		// Draw UI Elements
 		mainMenu.Draw(screen_in, 0, 0);
+
+		// Button Logic
 		if (startButton.isPressed()) { SetMenuState(LevelSelect); }
 
 		/* ======================== DEBUG ======================== */
@@ -22,9 +27,17 @@ void MenuManager::Draw(Tmpl8::Surface* screen_in) // Function that draws the req
 		/* ======================================================= */
 		break;
 	case LevelSelect:
-		// Draw & call actions that are needed
+		// Draw UI Elements
+		levelSelection.Draw(screen_in, 0, 0);
+
+		// Button Logic
+		if (levelOneButton.isPressed()) { /* Set Level-/GameState to Level/Playing */ }
+
+		/* ======================== DEBUG ======================== */
+		// levelOneButton.Draw(screen_in);
+		/* ======================================================= */
 		break;
 	}
 }
 
-void MenuManager::SetMenuState(MenuState state_in) { state = state_in; }
+void MenuManager::SetMenuState(MenuState state_in) { state = state_in; } // Function MenuState setter.
