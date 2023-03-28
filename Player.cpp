bac6172c69822d6a8216a03a3e7b822fbb9e92e3
SimenWol <68946439+MrGam3z@users.noreply.github.com>
2023-03-28 17:30:13 +0200

    Other: Player Class Cleanup - Added a few more variables to allow more control later on - Added debug functions in Player.Move() - Added more comments to the Player Class for clarity purposes

--- Player.cpp
+++ Player.cpp
@@ -23,31 +23,40 @@ namespace Tmpl8
 	{
 		float screenHeight = 512.0f; // Temp
 
+		// Update player location //
 		loc.x += speed.x * deltaTime;
-		float moveX = 1000.0f * deltaTime;
-
-		if (delta_loc.x == 0)
+		loc.y += speed.y * deltaTime;
+		
+		// Calculate new X speed //
+		float moveX = 1000.0f * playerSpeed * deltaTime;
+		if (delta_loc.x == 0) // Return speed to 0
 		{
 			if ((speed.x > -10.0f && speed.x < 0) || (speed.x < 10.0f && speed.x > 0)) { speed.x = 0; }
 			else if (speed.x > 0) { speed.x -= (moveX); }
 			else if (speed.x < 0) { speed.x += (moveX); }
 		}
-		else if (delta_loc.x == -1) { speed.x -= moveX; }
-		else if (delta_loc.x == 1) { speed.x += moveX; }
+		else if (delta_loc.x == -1) { speed.x -= moveX; }	// Increase speed to the left
+		else if (delta_loc.x == 1) { speed.x += moveX; }	// Increase speed to the right
 
-		if (speed.x <= -5000.0f) { speed.x = -5000.0f; }
-		else if (speed.x >= 5000.0f) { speed.x = 5000.0f; }
+		// Maximum speed
+		if (speed.x <= -maxPlayerSpeed) { speed.x = -maxPlayerSpeed; }
+		else if (speed.x >= maxPlayerSpeed) { speed.x = maxPlayerSpeed; }
 
-		std::cout << "speed.x: " << speed.x << "moveX: " << moveX << std::endl;
+		// Calculate new Y speed //
+		speed.y += (gravity * 1000.0f) * deltaTime;
 
-		loc.y += speed.y * deltaTime;
-		speed.y += (gravity * 1000.0f) * deltaTime; // CHANGE Y SPEED
-
+		// Make sure the player bounces on the bottom of the screen
 		if (loc.y > (screenHeight - static_cast<float>(player.GetHeight())))
 		{
 			loc.y = (screenHeight - static_cast<float>(player.GetHeight()));
 			speed.y = -(200.0f * bounceHeight);
 		}
+
+		/* ======================== DEBUG ======================== */
+		//std::cout << "Player Location: " << loc.x << ", " << loc.y << std::endl;	// Player Location
+		//std::cout << "speed.x: " << speed.x << " moveX: " << moveX << std::endl;	// SpeedX & MoveX
+		//std::cout << "speed.y: " << speed.y << std::endl;							// SpeedY
+		/* ======================================================= */
 	}
 
 	void Player::Death()
--- Player.h
+++ Player.h
@@ -15,10 +15,13 @@ namespace Tmpl8
 		void Death();	// Runs everything necessary on death
 		// CheckCollision, etc.
 	private:
-		Location loc = { 20.0f, 400.0f };	// Location of the player
-		Location speed = { 0.0f, 0.0f };	// Speed of the player
-		float gravity = 1.0f;				// Level of gravity
-		float bounceHeight = 1.0f;			// Level of bounce height
+		Location loc = { 20.0f, 400.0f };			// Current location of the player
+		Location speed = { 0.0f, 0.0f };			// Current speed of the player
+
+		static constexpr float gravity = 1.0f;			// Level of gravity
+		static constexpr float bounceHeight = 1.0f;		// Level of bounce height
+		static constexpr float playerSpeed = 1.0f;		// Level of player speed
+		static constexpr float maxPlayerSpeed = 500.0f;	// Maximum allowed player speed (X direction only)
 	};
 
 };
